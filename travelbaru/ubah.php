4bde8c853f78858babab243c0a52d478f7f4752f
chikitachika <55478565+chikitachika@users.noreply.github.com>
2019-11-19 15:22:42 +0700

    asyique

--- travelbaru/ubah.php
+++ travelbaru/ubah.php
@@ -39,26 +39,35 @@ if( isset($_POST["submit"]) ){
 <!DOCTYPE html>
 <html>
 <head>
+<style>
+table {
+  margin: auto;
+  width: 40%;
+  border: 3px solid #73AD21;
+  padding: 10px;
+}
+</style>
+    
     <title>Ubah Data Pengguna</title>
 
 </head>
 
 <body>
-    <h1> Tambah Data Pengguna<h1>
+    <center><h1> Tambah Data Pengguna<h1></center>
 
     <form action="" method ="post">
-    <table width="25%" border="0">
+    <table width="35%" border="0">
             <input type="hidden" name="id_user" value="<?= $user["id_user"]; ?>"
             <tr> 
-                <td><label for="nama">Nama</label></td>
+                <td>Nama</td>
                 <td><input type="text"  class="form_login" id ="nama" name="nama" autocomplete="off" required value="<?= $user["nama"]; ?>"></td>
             </tr>
             <tr> 
-                <td><label for="tmpt">Tempat Lahir</td>
+                <td>Tempat Lahir</td>
                 <td><input type="text"  class="form_login" id="tmpt" name="tempat_lahir" autocomplete="off" required value="<?= $user["tempat_lahir"]; ?>" ></td>
             </tr>
             <tr> 
-                <td><label for="tgl">Tanggal Lahir</td>
+                <td>Tanggal Lahir</td>
                 <td><input type="date"  class="form_login" id="tgl" name="tanggal_lahir" autocomplete="off" required value="<?= $user["tanggal_lahir"]; ?>"></td>
             </tr>
             <tr> 
@@ -76,27 +85,27 @@ if( isset($_POST["submit"]) ){
             </tr>
             
             <tr> 
-                <td><label for="nod">Nomer ID</td>
+                <td>Nomer ID</td>
                 <td><input type="text" maxlength="16" onkeypress="return hanyaAngka(event)" class="form_login" id="nod" name="nomer_id" autocomplete="off" required value="<?= $user["nomer_id"]; ?>"></td>
             </tr>
             <tr> 
-                <td><label for="telepon">Telepon</td>
+                <td>Telepon</td>
                 <td><input type="text" maxlength="13" onkeypress="return hanyaAngka(event)" class="form_login" id="telepon" name="telepon" autocomplete="off"  required value="<?= $user["telepon"]; ?>"></td>
             </tr>
             <tr> 
-                <td><label for="alamat">Alamat</td>
+                <td>Alamat</td>
                 <td><input type="text"  class="form_login" id="alamat" name="alamat" autocomplete="off" required value="<?= $user["alamat"]; ?>"></td>
             </tr>
             <tr> 
-                <td><label for="email">Email</td>
+                <td>Email</td>
                 <td><input type="text"  class="form_login" id="email" name="email" autocomplete="off" required value="<?= $user["email"]; ?>"></td>
             </tr>
             <tr> 
-                <td><label for="username">Username</td>
+                <td>Username</td>
                 <td><input type="text"  class="form_login" id="username" name="username" autocomplete="off" required value="<?= $user["username"]; ?>"></td>
             </tr>
             <tr> 
-                <td><label for="password">Password</td>
+                <td>Password</td>
                 <td><input type="password"  class="form_login" id="password" name="password" autocomplete="off" required value="<?= $user["password"]; ?>"></td>
             </tr>
             <tr> 
